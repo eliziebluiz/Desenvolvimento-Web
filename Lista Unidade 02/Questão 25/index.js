a1601d2ed0686282efc6a0d7d07f952c18a8e976
eliziebluiz <elizieb.l3@gmail.com>
2022-09-24 19:56:05 -0300

    fix: add question 26;

---
+++ Lista Unidade 02/Questão 25/index.js
@@ -0,0 +1,41 @@
+class Autor {
+  constructor(nameAuthor, ano) {
+    this.nameAuthor = nameAuthor;
+    this.ano = ano;
+  }
+}
+
+class Livro extends Autor {
+  constructor(name, gender, nameAuthor, ano) {
+    super(nameAuthor, ano);
+    this.name = name;
+    this.gender = gender;
+  }
+}
+
+function temMesmoAutor(autorone, autortwo) {
+  return autorone === autortwo;
+}
+
+function main() {
+  let name = prompt("Digite o nome do livro:");
+  let autor = prompt("Digite o nome autor:");
+  let ano = prompt("Digite o ano do autor:");
+  let gender = prompt("Digite o gênero do livro:");
+
+  let livro = new Livro(name, gender, autor, ano);
+
+  console.log(livro);
+
+  if (!!livro.ano && !!livro.name && !!livro.gender && !!livro.nameAuthor) {
+    let autordois = prompt("Digite o nome do segundo autor:");
+    let igualsAutor = temMesmoAutor(autordois, livro.nameAuthor);
+
+    alert(JSON.stringify(livro, null, 2));
+    if (igualsAutor) {
+      alert("O autor é o mesmo");
+    }
+  }
+}
+
+main();
